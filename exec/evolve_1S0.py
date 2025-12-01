3e749c4b12958e1befe0640198983c1c525189e7
Matthias Heinz <heinzmc@ornl.gov>
2025-12-01 10:04:41 -0500

    Identify todos for evolving new potentials

--- exec/evolve_1S0.py
+++ exec/evolve_1S0.py
@@ -3,3 +3,10 @@
 # This software is released under the MIT License.
 # https://opensource.org/licenses/MIT
 
+# 1 Load potential
+
+# 2 Compute binding energy
+
+# 3 SRG evolve
+
+# 4 Compute binding energy

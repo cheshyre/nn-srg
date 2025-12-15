8c8c70cb011684098c86d2df533959e21030117f
Matthias Heinz <heinzmc@ornl.gov>
2025-12-15 15:33:14 -0500

    Add constants

---
+++ nn_srg/constants.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2025 Matthias Heinz
+# 
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+# Physical constants
+hbarc = 197.327
+proton_mass = 938.272
+neutron_mass = 939.565
+red_mass = proton_mass * neutron_mass / (proton_mass + neutron_mass)
